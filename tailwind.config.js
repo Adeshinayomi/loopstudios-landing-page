12a8457c42bfb824ee1048f8575847721016f309
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-03-10 21:16:32 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        White: "hsl(0, 0%, 100%)",
+        Black: "hsl(0, 0%, 0%)",
+        Grey200: "hsl(0, 0%, 85%)"
+      },
+      fontFamily:{
+        alata: ["Alata", "sans-serif"],
+        josefin:["Josefin Sans", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
