10ba91eb5019225417aaa9dbd7a1be5d848ef3e3
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-03-14 15:39:56 +0100

    nav section creation for desktop

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,9 +2,18 @@ import logo from '../assets/images/logo.svg'
 import hamburger from '../assets/images/icon-hamburger.svg'
 export function NavBar(){
     return(
-        <nav  className='w-full fixed top-0 px-4 py-10 flex justify-between'>
-            <img src={logo} alt="logo" />
-            <img src={hamburger} alt="hamburger icon" width={40}/>
+        <nav className='w-full fixed top-0 px-4 py-10'>
+            <div className=' flex justify-between md:w-5/6 mx-auto'>
+                <img src={logo} alt="logo" />
+                <ul className='hidden text-white justify-self-center font-alata md:flex gap-5 justify-center'>
+                    <li className='text-lg md:text-sm font-normal text-center'>About</li>
+                    <li className='text-lg md:text-sm  font-normal text-center'>Careers</li>
+                    <li className='text-lg md:text-sm  font-normal text-center'>Events</li>
+                    <li className='text-lg md:text-sm font-normal text-center'>Products</li>
+                    <li className='text-lg md:text-sm  font-normal text-center'>Support</li>
+                </ul>
+                <img src={hamburger} alt="hamburger icon" width={40} className='md:hidden'/>
+            </div>
         </nav>
     )
 }
